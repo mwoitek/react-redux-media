a91e171da6f5e20fc788ac8862b7c71d1757e035
Marcio Woitek <mwoitekjr@gmail.com>
2023-04-17 00:52:13 -0300

    Connect React to Redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
+import { store } from './store';
 
 const el = document.getElementById('root');
 const root = createRoot(el);
 
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);

a33e94c5504e419a42939205e2073bcbe9d01e19
Marcio Woitek <mwoitekjr@gmail.com>
2023-04-19 18:40:06 -0300

    Partial implementation of 'Remove Album' feature

--- src/store/apis/albumsApi.js
+++ src/store/apis/albumsApi.js
@@ -48,9 +48,22 @@ const albumsApi = createApi({
           };
         },
       }),
+
+      removeAlbum: builder.mutation({
+        query: (album) => {
+          return {
+            url: `/albums/${album.id}`,
+            method: 'DELETE',
+          };
+        },
+      }),
     };
   },
 });
 
-export const { useFetchAlbumsQuery, useAddAlbumMutation } = albumsApi;
+export const {
+  useFetchAlbumsQuery,
+  useAddAlbumMutation,
+  useRemoveAlbumMutation,
+} = albumsApi;
 export { albumsApi };
